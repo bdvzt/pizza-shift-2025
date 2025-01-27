bec30b870a4a63a2a31843be59c0db39e217245c
bdvzt <budaevazay@mail.ru>
2025-01-27 19:40:22 +0800

    Initial commit

---
+++ PizzaDelivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PizzaDelivery
+//
+//  Created by Zayata Budaeva on 27.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
